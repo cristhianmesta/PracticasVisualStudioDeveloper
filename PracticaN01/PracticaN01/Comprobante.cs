12b65a427f05857e351ed901241dbea13bfe1c5c
Cristhian <Cristhian@CRISTHIAN-NB>
2016-07-24 16:32:29 -0500

    POO

---
+++ PracticaN01/PracticaN01/Comprobante.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaN01
+{
+    abstract class Comprobante
+    {
+        protected int NumeroDocumento { get;  set; }
+        protected DateTime FechaEmision { get; set; }
+        protected string RazonSocial { get; set; }
+
+        protected List<int> Unidades { get; set; }
+        protected List<string> Descripcion { get; set; }
+        protected List<double> Precios { get; set; }
+        protected List<double> Importes { get; set; }
+
+        public Comprobante()
+        {
+            Descripcion = new List<string>();
+            Unidades = new List<int>();
+            Precios = new List<double>();
+            Importes = new List<double>();
+        }
+
+        public void AgregarDetalle(int tipo, int iunidades, string idescripcion, double imonto)
+        {
+            Descripcion.Add(idescripcion);
+            Precios.Add(imonto);
+            Unidades.Add(iunidades);
+            switch (tipo)
+            {
+                case 1:
+                    Importes.Add(iunidades * imonto);
+                    break;
+                case 2:
+                    Importes.Add(iunidades * imonto);
+                    break;
+                case 3:
+                    Importes.Add(imonto);
+                    break;
+            }
+        }
+
+        public void MonstrarDetalle(int tipo)
+        {
+            for (int i = 0; i < Descripcion.Count; i++)
+            {
+                switch (tipo) {
+                    case 1:
+                        Console.WriteLine($"{i + 1}. ({Unidades[i]} UND.) {Descripcion[i]} \t  S/. {Precios[i].ToString("F")} \t S/. {Importes[i].ToString("F")}");
+                        break;
+                    case 2:
+                        Console.WriteLine($"{i + 1}. ({Unidades[i]} UND.) {Descripcion[i]} \t  S/. {Precios[i].ToString("F")} \t S/. {Importes[i].ToString("F")}");
+                        break;
+                    case 3:
+                        Console.WriteLine($"{Descripcion[i]} \t S/. {Importes[i].ToString("F")}");
+                        break;
+
+                }
+            } 
+        }
+
+        protected double SubTotal(int tipo) {
+            double Suma = 0;
+            for (int i = 0; i < Descripcion.Count; i++)
+            {
+                Suma = Suma + Importes[i];
+            }
+            return Suma;
+        }
+
+        public abstract double Impuesto();
+        public abstract double Total();
+        public abstract void Imprimir();
+
+    }
+
+    class Boleta : Comprobante
+    {
+
+        public Boleta(int numerodocumento, DateTime fechaemision, string razonsocial)
+        {
+            this.NumeroDocumento = numerodocumento;
+            this.FechaEmision = fechaemision;
+            this.RazonSocial = razonsocial;
+            //this.Descripcion = descripcion;
+            //this.Importes = importes;
+        }
+
+        public override double Impuesto()
+        {
+            return 0;
+        }
+
+        public override double Total()
+        {
+            return SubTotal(2) + Impuesto();
+        }
+
+        public override void Imprimir()
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine($"N° {NumeroDocumento}");
+            Console.WriteLine($"Razón Social: {RazonSocial}");
+            Console.WriteLine($"F. Emisión: {FechaEmision}");
+            Console.WriteLine(string.Concat(Enumerable.Repeat("=", 70)));
+            MonstrarDetalle(2);
+            Console.WriteLine(string.Concat(Enumerable.Repeat("=", 70)));
+            Console.WriteLine($"TOTAL \t\t: S/. {Total().ToString("F")}");
+        }
+    }
+
+    class Factura : Comprobante
+    {
+        private DateTime FechaVencimiento { get; set; }
+        private string Ruc { get; set; }
+
+        public Factura(int numerodocumento, DateTime fechaemision, DateTime fechavencimiento, string ruc, string razonsocial)
+        {
+            this.NumeroDocumento = numerodocumento;
+            this.FechaEmision = fechaemision;
+            this.FechaVencimiento = fechavencimiento;
+            this.Ruc = ruc;
+            this.RazonSocial = razonsocial;
+            //this.Descripcion = descripcion;
+            //this.Importes = importes;
+        }
+
+        public override double Impuesto()
+        {
+            return SubTotal(1) * 0.18;
+        }
+
+        public override double Total()
+        {
+            return SubTotal(1) + Impuesto();
+        }
+
+        public override void Imprimir()
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine($"N° {NumeroDocumento}");
+            Console.WriteLine($"RUC: {Ruc}");
+            Console.WriteLine($"Razón Social: {RazonSocial}");
+            Console.WriteLine($"F. Emisión: {FechaEmision}");
+            Console.WriteLine($"F. Vencimiento: {FechaVencimiento}");
+            Console.WriteLine(string.Concat(Enumerable.Repeat("=",70)));
+            MonstrarDetalle(1);
+            Console.WriteLine(string.Concat(Enumerable.Repeat("=", 70)));
+            Console.WriteLine($"SUB TOTAL \t: S/. {SubTotal(2).ToString("F")}");
+            Console.WriteLine($"IMPUESTO (18%) \t: S/. {Impuesto().ToString("F")}");
+            Console.WriteLine($"TOTAL \t\t: S/. {Total().ToString("F")}");
+        }
+    }
+
+    class Recibo : Comprobante
+    {
+        private string Ruc { get;  set; }
+
+        public Recibo(int numerodocumento, DateTime fechaemision, string ruc, string razonsocial)
+        {
+            this.NumeroDocumento = numerodocumento;
+            this.FechaEmision = fechaemision;
+            this.Ruc = ruc;
+            this.RazonSocial = razonsocial;
+            //this.Descripcion = descripcion;
+            //this.Importes = importes;
+        }
+
+        public override double Impuesto()
+        {
+            return SubTotal(3) * 0.10;
+        }
+
+        public override double Total()
+        {
+            return SubTotal(3) - Impuesto();
+        }
+
+        public override void Imprimir()
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine($"N° {NumeroDocumento}");
+            Console.WriteLine($"RUC: {Ruc}");
+            Console.WriteLine($"Razón Social: {RazonSocial}");
+            Console.WriteLine($"F. Emisión: {FechaEmision}");
+            Console.WriteLine(string.Concat(Enumerable.Repeat("=", 70)));
+            MonstrarDetalle(3);
+            Console.WriteLine(string.Concat(Enumerable.Repeat("=", 70)));
+            Console.WriteLine($"SUB TOTAL \t: {SubTotal(3).ToString ("F")}");
+            Console.WriteLine($"IMPUESTO (10%) \t: {Impuesto().ToString("F")}");
+            Console.WriteLine($"TOTAL \t\t: S/. {Total().ToString("F")}");
+        }
+    }
+}
---
+++ PracticaN01/PracticaN01/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaN01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int otrocomprobante=0;
+            do
+            {
+
+                Console.Write("Tipo de documento: " + "\n" + "[1] Factura" + "\n" + "[2] Boleta" + "\n" + "[3] Recibo" + "\n" + "Ingresar opción: ");
+                var tipo = Console.ReadLine();
+                switch (tipo.ToString().Trim())
+                {
+                    case "1":
+                        IngresarFactura();
+                        break;
+                    case "2":
+                        IngresarBoleta();
+                        break;
+                    case "3":
+                        IngresarRecibo();
+                        break;
+                }
+                Console.Write("\n");
+                Console.WriteLine("¿Desea ingresar otro comprobante? [1] Sí [2]No ");
+                Console.Write("Ingresar opción:");
+                otrocomprobante = int.Parse(Console.ReadLine());
+            } while (otrocomprobante == 1);
+            Console.ReadKey();
+        }
+
+        static void IngresarFactura()
+        {
+            int numerodocumento;
+            DateTime fechaemision;
+            DateTime fechavencimiento;
+            string ruc;
+            string razonsocial;
+
+            Console.Write("\n");
+            Console.Write("Ingrese el número de la factura: ");
+            numerodocumento = int.Parse (Console.ReadLine());
+
+            Console.Write("Ingrese la fecha de emisión: ");
+            fechaemision = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese la fecha de vencimiento: ");
+            fechavencimiento  = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el número de ruc: ");
+            ruc  = Console.ReadLine().Trim();
+
+            Console.Write("Ingrese la razón social: ");
+            razonsocial  = Console.ReadLine().Trim();
+
+            Factura oFactura = new Factura(numerodocumento,fechaemision, fechavencimiento, ruc, razonsocial);
+            Comprobante oComprobante = oFactura as Factura;
+            AgregarDetalle(1, oComprobante);
+
+            oFactura.Imprimir();
+        }
+
+        static void IngresarBoleta()
+        {
+            int numerodocumento;
+            DateTime fechaemision;
+            string razonsocial;
+
+            Console.Write("\n");
+            Console.Write("Ingrese el número de la Boleta: ");
+            numerodocumento = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese la fecha de emisión: ");
+            fechaemision = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese la razón social: ");
+            razonsocial = Console.ReadLine().Trim();
+
+            Boleta oBoleta = new Boleta (numerodocumento, fechaemision, razonsocial);
+            Comprobante oComprobante = oBoleta as Boleta ;
+            AgregarDetalle(2, oComprobante);
+
+            oBoleta.Imprimir();
+        }
+
+        static void IngresarRecibo()
+        {
+            int numerodocumento;
+            DateTime fechaemision;
+            string ruc;
+            string razonsocial;
+
+            Console.Write("\n");
+            Console.Write("Ingrese el número del recibo: ");
+            numerodocumento = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese la fecha de emisión: ");
+            fechaemision = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el número de ruc: ");
+            ruc = Console.ReadLine().Trim();
+
+            Console.Write("Ingrese la razón social: ");
+            razonsocial = Console.ReadLine().Trim();
+
+            Recibo oRecibo = new Recibo (numerodocumento, fechaemision, ruc, razonsocial);
+            Comprobante oComprobante = oRecibo as Recibo;
+            AgregarDetalle(3, oComprobante);
+
+            oRecibo.Imprimir();
+        }
+
+        static void AgregarDetalle(int tipo, Comprobante objeto) {
+            int otrodetalle=0;
+            do
+            {
+                Console.Write("\n");
+                Console.WriteLine("Agregar item:");
+
+                Console.Write("Descripción: ");
+                string descripcion = Console.ReadLine().Trim();
+
+                int unidades = 1;
+                if (tipo != 3)
+                {
+                    Console.Write("Cantidad: ");
+                    unidades  = int.Parse(Console.ReadLine().Trim());
+                }
+
+                Console.Write("Precio: ");
+                double monto = double.Parse(Console.ReadLine().Trim());
+
+                objeto.AgregarDetalle(tipo, unidades, descripcion, monto);
+
+                if (tipo < 3) {
+                    Console.WriteLine("¿Desea ingresar otro detalle? [1] Sí [2]No");
+                    Console.Write("Ingresar opción:");
+                    otrodetalle = int.Parse(Console.ReadLine());
+                }
+            } while (otrodetalle == 1);
+        }
+
+    }
+}
